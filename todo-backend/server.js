7d622c05b275373ab1649c45849042816463f6c7
rohit-ku-patel <rp58007@gmail.com>
2025-08-20 11:38:29 +0530

    added

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -18,7 +18,8 @@ const isNonEmpty = (v) => typeof v === 'string' && v.trim().length > 0;
 
 // --- Database Connection ---
 const pool =  mysql.createPool({
-  host: process.env.DB_HOST,
+    host: process.env.DB_HOST,
+  port: process.env.DB_PORT,       // important!
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
